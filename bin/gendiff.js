1bb3dd9649721ea81c714efa79d3edd45554c151
KDI <kdi.course@gmail.com>
2021-03-05 13:17:41 +0200

    add list of options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { Command, Option } from 'commander';
 import genDiff from '../src/gendiff.js';
+
+const formatOption = new Option('-f, --format [type]', 'output format')
+  .choices(['stylish', 'plain'])
+  .default('stylish');
 
 const program = new Command();
 
@@ -8,7 +12,7 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .addOption(formatOption)
   .action((filepath1, filepath2, options) => {
     const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
